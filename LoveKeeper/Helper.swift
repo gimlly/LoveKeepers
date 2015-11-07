64ba1dd5aaa29ab2a3755415da0a32eb89560705
Matej Kusnier <matej@Matejs-MacBook-Pro.local>
2015-11-07 21:35:01 +0100

    first commit

---
+++ LoveKeeper/Helper.swift
@@ -0,0 +1,13 @@
+//
+//  Helper.swift
+//  LoveKeeper
+//
+//  Created by Matej Kusnier on 11/7/15.
+//  Copyright © 2015 LoveKeepers. All rights reserved.
+//
+
+import UIKit
+
+class Helper: NSObject {
+
+}
